14d4c36f40f64fac0d6cddcc9df9f72c4f8f3f22
Curtis <curtismensah48@gmail.com>
2021-03-22 15:14:00 +0000

    configuration for tailwind & postcss

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,10 @@
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,300;1,400&family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap');
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  /* font-family: 'Roboto', 'Space Mono', 'Avenir', 'Helvetica', 'Arial', 'sans-serif'; */
 }
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
     purge: [
         './src/**/*.html',
@@ -6,7 +8,35 @@ module.exports = {
     ],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        colors: {
+            primary: colors.red,
+            blue: colors.lightBlue,
+            dark: colors.black,
+            pink: colors.fuchsia,
+        },
+
+        backgroundColor: {
+            'primary': '#070B0D',
+            'secondary': '#82DD03',
+            'danger': '#222831',
+        },
+
+        textColor: {
+            'primary': '#ffffff',
+            'secondary': '#070B0D',
+            'danger': '#82DD03',
+            'danger-500': '#82dd03be',
+        },
+        
+        fontFamily: {
+            'display': ['Roboto', 'Space Mono', 'Avenir', 'Helvetica', 'Arial', 'sans-serif'],        
+            'body': ['Roboto', 'Space Mono', 'Avenir', 'Helvetica', 'Arial', 'sans-serif'],
+        },
+        extend: {
+            textColor: ['active'],
+            fontFamily: ['active'],
+            colors: ['active'],
+        },
     },
     variants: {},
     plugins: [],
